6c2fba53f5812bd4cb50af80de6f83fa3b6df6d1
leonardo <leonardo@leonardo-VirtualBox>
2022-12-15 20:15:07 -0300

    Adicionando documentação do CRUD com Jedis.

--- Banco_de_Dados/CRUD-Java/jredis/Utils.java
+++ Banco_de_Dados/CRUD-Java/jredis/Utils.java
@@ -11,8 +11,14 @@ import redis.clients.jedis.exceptions.JedisConnectionException;
 public class Utils {
 	
 	static Scanner teclado = new Scanner(System.in);
+	//Para o Redis é necessário um software de conexão. Nesse caso usou-se o Jedis, disponível na pasta 'libs' do projeto.
 	
+	/**
+	 * Gera uma chave de ID para o dado inserido no Redis.
+	 * @return
+	 */
 	public static String gera_id() {
+		//Conexão.
 		Jedis con = conectar();
 		
 		String chave = con.get("chave");
@@ -30,29 +36,46 @@ public class Utils {
 		}
 	}
 	
+	/**
+	 * Realiza a conexão com o Redis.
+	 * @return Conexão
+	 */
 	public static Jedis conectar() {
 		Jedis con = new Jedis("redis://localhost:6379");
 		
 		return con;
 	}
 	
+	/**
+	 * Realiza a desconexão com o Redis.
+	 * @param Conexão
+	 */
 	public static void desconectar(Jedis con) {
 		con.disconnect();
 	}
 	
+	/**
+	 * Lista os dados já inseridos no Redis
+	 */
 	public static void listar() {
+		//Conexão.
 		Jedis con = conectar();
 		
+		//Tratamento de exceções.
 		try {
+			//Recuperando todas as chaves do database.
 			Set<String> res = con.keys("produtos*");
 			
+			//Se res > 0, existem chaves.
 			if(res.size() > 0) {
 				System.out.println("\n================Produtos==============\n");
 				
+				//Listando os produtos.
 				for(String key : res) {
+					//Mapeia as chaves e seus valores.
 					Map<String, String> product = con.hgetAll(key);
 					
-					System.out.println("ID: " + key);
+					System.out.println("\nID: " + key);
 					System.out.println("Produto: " + product.get("nome"));
 					System.out.println("Preço: " + product.get("preco"));
 					System.out.println("Estoque: " + product.get("estoque"));
@@ -62,16 +85,22 @@ public class Utils {
 					System.out.println("\nAinda não há produtos cadastrados.\n");
 			}
 		}catch(JedisConnectionException e) {
-			System.out.println("Verifique se o servidor está ativo.\n");
+			System.out.println("\nVerifique se o servidor está ativo.\n");
 			e.printStackTrace();
 		}
 		
 		menu();
 	}
 	
+	/**
+	 * Insere novos dados no Redis.
+	 */
 	public static void inserir() {
+		//Conexão.
 		Jedis con = conectar();
 		
+		//Recebendo os dados a serem inseridos no Redis.
+		//Nota: O Redis só trabalha com o formato String.
 		System.out.println("Insira o nome do produto: ");
 		String nome = teclado.nextLine();
 		System.out.println("Insira o preço do produto: ");
@@ -79,34 +108,48 @@ public class Utils {
 		System.out.println("Insira a quantidade em estoque do produto: ");
 		String estoque = teclado.nextLine();
 		
+		//Mapeando as chaves e valores.
 		Map<String, String> product = new HashMap<String, String>();
 		
+		//Integrando as chaves com os valores informados pelo usuário.
 		product.put("nome", nome);
 		product.put("preco", preco);
 		product.put("estoque", estoque);
 		
+		//
 		String chave = "produtos" + Utils.gera_id();
 		
+		//Tratamento de exceções.
 		try {
+			//Retorno da chave e do valor.
 			String res = con.hmset(chave, product);
 			
 			if(res != null) {
 				System.out.println("\nProduto " + nome + " adicionado com sucesso.\n");
 				menu();
+			}else {
+				System.out.println("\nNão foi possível inserir o produto.\n");
 			}
 		}catch(JedisConnectionException e) {
 			System.out.println("Verifique se o Redis está ativo.");
 			e.printStackTrace();
 		}
-		//desconectar(con);
+		
+		desconectar(con);
 	}
 	
+	/**
+	 * Atualiza os dados do Redis.
+	 */
 	public static void atualizar() {
+		//Conexão.
 		Jedis con = conectar();
 		
+		//Recebendo a chave do dado a ser atualizado.
 		System.out.println("Informe a chave do produto: ");
 		String chave = teclado.nextLine();
 		
+		//Recebendo os novos valores do dado da chave informada.
 		System.out.println("Insira o novo nome do produto: ");
 		String nome = teclado.nextLine();
 		System.out.println("Insira o novo preço do produto: ");
@@ -114,19 +157,23 @@ public class Utils {
 		System.out.println("Insira a nova quantidade em estoque do produto: ");
 		String estoque = teclado.nextLine();
 		
+		//Mapeando as chaves e valores.
 		Map<String, String> product = new HashMap<String, String>();
 		
+		//Integrando as chaves e valores.
 		product.put("nome", nome);
 		product.put("preco", preco);
 		product.put("estoque", estoque);
 		
+		//Tratamento de exceções.
 		try {
+			//Retorno da chave-valor.
 			String res = con.hmset(chave, product);
 			
 			if(res != null) {
 				System.out.println("\nProduto " + nome + " atualizado com sucesso.\n");
 			}else {
-				System.out.println("Não foi possível atualizar o produto.");
+				System.out.println("\nNão foi possível atualizar o produto.\n");
 			}
 		}catch(JedisConnectionException e) {
 			System.out.println("Verifique se o Redis está ativo.");
@@ -137,13 +184,20 @@ public class Utils {
 		menu();
 	}
 	
+	/**
+	 * Deleta dados do Redis.
+	 */
 	public static void deletar() {
+		//Conexão.
 		Jedis con = conectar();
 		
+		//Recebendo a chave do dado a ser deletado.
 		System.out.println("Informe a chave do produto: ");
 		String chave = teclado.nextLine();
 		
+		//Tratamento de exceções.
 		try {
+			//Variável do tipo Long pois o método del pode deletar milhões de dados de uma só vez.
 			Long delet = con.del(chave);
 			
 			if(delet > 0) {
@@ -152,7 +206,7 @@ public class Utils {
 				System.out.println("\nNão há produtos com a chave informada.\n");
 			}
 		}catch(JedisConnectionException e) {
-			System.out.println("Verifique se o Redis está ativo: ");
+			System.out.println("\nVerifique se o Redis está ativo.\n");
 			e.printStackTrace();
 		}
 		desconectar(con);
@@ -178,7 +232,8 @@ public class Utils {
 		}else if(opcao == 4) {
 			deletar();
 		}else {
-			System.out.println("Opção inválida.");
+			System.out.println("\nOpção inválida.\n");
+			menu();
 		}
 	}
 }
